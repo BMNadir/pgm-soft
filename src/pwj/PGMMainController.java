937dc1a0127e9c47d7a9eb0644aee6298883bd2b
Nadir <bmnadir2@gmail.com>
2019-02-04 16:32:43 +0100

    Added Flash & EEPROM UI tables

--- src/pwj/PGMMainController.java
+++ src/pwj/PGMMainController.java
@@ -3,11 +3,17 @@ package pwj;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
@@ -21,6 +27,8 @@ public class PGMMainController implements Initializable, iCommands{
     private boolean programmerFound = false;
     private String hexLastPath = "";
     private File hexFile;
+    ObservableList<MemoryDumpRow> flashList = FXCollections.observableArrayList ();
+    ObservableList<MemoryDumpRow> eepromList = FXCollections.observableArrayList ();
     
     @FXML
     private StackPane rootPane = new StackPane ();
@@ -36,11 +44,128 @@ public class PGMMainController implements Initializable, iCommands{
     private MenuItem connectMenuItem = new MenuItem();
     @FXML
     private MenuItem disconnectMenuItem = new MenuItem();
+    @FXML
+    private TableView<MemoryDumpRow> flashTable = new TableView<MemoryDumpRow>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddrCol = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr0 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr1 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr2 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr3 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr4 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr5 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr6 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableColumn<MemoryDumpRow, String> flashAddr7 = new TableColumn<MemoryDumpRow, String>();
+    @FXML
+    private TableView<MemoryDumpRow> eepromTable;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddrCol;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr0;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr1;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr2;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr3;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr4;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr5;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr6;
+    @FXML
+    private TableColumn<MemoryDumpRow, String> eepromAddr7;
     
+    public static class MemoryDumpRow 
+    {
+        private static int addr = -8; //So that the first line has the address 0
+        private final SimpleStringProperty addrCol;
+        private final SimpleStringProperty addr0;
+        private final SimpleStringProperty addr1;
+        private final SimpleStringProperty addr2;
+        private final SimpleStringProperty addr3;
+        private final SimpleStringProperty addr4;
+        private final SimpleStringProperty addr5;
+        private final SimpleStringProperty addr6;
+        private final SimpleStringProperty addr7;
+
+        public MemoryDumpRow(String addr0, String addr1, String addr2, String addr3, String addr4, String addr5, String addr6, String addr7) 
+        {
+            addr += 8;
+            this.addrCol = new SimpleStringProperty (Integer.toHexString(addr));
+            this.addr0 = new SimpleStringProperty (addr0);
+            this.addr1 = new SimpleStringProperty (addr1);
+            this.addr2 = new SimpleStringProperty (addr2);
+            this.addr3 = new SimpleStringProperty (addr3);
+            this.addr4 = new SimpleStringProperty (addr4);
+            this.addr5 = new SimpleStringProperty (addr5);
+            this.addr6 = new SimpleStringProperty (addr6);
+            this.addr7 = new SimpleStringProperty (addr7);
+        }
+
+        public String getAddrCol() {
+            return addrCol.get().toUpperCase();
+        }
+
+        public String getAddr0() {
+            return addr0.get();
+        }
+
+        public String getAddr1() {
+            return addr1.get();
+        }
+
+        public String getAddr2() {
+            return addr2.get();
+        }
+
+        public String getAddr3() {
+            return addr3.get();
+        }
+
+        public String getAddr4() {
+            return addr4.get();
+        }
+
+        public String getAddr5() {
+            return addr5.get();
+        }
+
+        public String getAddr6() {
+            return addr6.get();
+        }
+
+        public String getAddr7() {
+            return addr7.get();
+        }
+    }
     
+    /**************************************************************************/
+    /**************************************************************************/
+    /**************************************************************************/
+    /**                             METHODS                                  **/
+    /**************************************************************************/
+    /**************************************************************************/
+    /**************************************************************************/
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         connectMenuItem.fire(); //Start connectToProgrammer routine
+        initFlashDump ();
+        MemoryDumpRow.addr = -8; 
+        initEEPROMDump ();
+        flashAddrCol.getStyleClass().add("address-column"); //Change background color of address column in Flash memory dump
+        flashTable.getStyleClass().add("noheader"); //Remove column headers from Flash memory dump
+        eepromAddrCol.getStyleClass().add("address-column"); //Change background color of address column in EEPROM memory dump
+        eepromTable.getStyleClass().add("noheader"); //Remove column headers from EEPROM memory dump
     }    
 
     @FXML
@@ -116,9 +241,46 @@ public class PGMMainController implements Initializable, iCommands{
         USBFunctions.usbTerminate();
         connectMenuItem.setDisable(false);
         disconnectMenuItem.setDisable(true);
+        setProgrammerFound(false);
         programmerStatus.setText("Programmateur déconnecté");
         PwJ.setUsbFound(false);
     }
     
+    private void initFlashDump () 
+    {
+        flashAddrCol.setCellValueFactory(new PropertyValueFactory<>("addrCol")); //Associate addr member of MemoryDumpRow with flashAddrCol table column
+        flashAddr0.setCellValueFactory(new PropertyValueFactory<>("addr0")); 
+        flashAddr1.setCellValueFactory(new PropertyValueFactory<>("addr1")); 
+        flashAddr2.setCellValueFactory(new PropertyValueFactory<>("addr2")); 
+        flashAddr3.setCellValueFactory(new PropertyValueFactory<>("addr3")); 
+        flashAddr4.setCellValueFactory(new PropertyValueFactory<>("addr4")); 
+        flashAddr5.setCellValueFactory(new PropertyValueFactory<>("addr5")); 
+        flashAddr6.setCellValueFactory(new PropertyValueFactory<>("addr6")); 
+        flashAddr7.setCellValueFactory(new PropertyValueFactory<>("addr7")); 
+        
+        for (int i = 0; i < 50; i++)
+        {
+            flashList.add(new MemoryDumpRow("FFFF", "FFFF", "FFFF", "FFFF", "FFFF", "FFFF", "FFFF", "FFFF"));
+        }
+        flashTable.getItems().setAll(flashList);
+    }
     
+    private void initEEPROMDump ()
+    {
+        eepromAddrCol.setCellValueFactory(new PropertyValueFactory<>("addrCol")); //Associate addr member of MemoryDumpRow with flashAddrCol table column
+        eepromAddr0.setCellValueFactory(new PropertyValueFactory<>("addr0")); 
+        eepromAddr1.setCellValueFactory(new PropertyValueFactory<>("addr1")); 
+        eepromAddr2.setCellValueFactory(new PropertyValueFactory<>("addr2")); 
+        eepromAddr3.setCellValueFactory(new PropertyValueFactory<>("addr3")); 
+        eepromAddr4.setCellValueFactory(new PropertyValueFactory<>("addr4")); 
+        eepromAddr5.setCellValueFactory(new PropertyValueFactory<>("addr5")); 
+        eepromAddr6.setCellValueFactory(new PropertyValueFactory<>("addr6")); 
+        eepromAddr7.setCellValueFactory(new PropertyValueFactory<>("addr7")); 
+        
+        for (int i = 0; i < 20; i++)
+        {
+            eepromList.add(new MemoryDumpRow("FFFF", "FFFF", "FFFF", "FFFF", "FFFF", "FFFF", "FFFF", "FFFF"));
+        }
+        eepromTable.getItems().setAll(eepromList);
+    }
 }
